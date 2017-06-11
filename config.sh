c34defb0b7d240f39153ad8d3167d504017c150e
Aaditya Bagga <aaditya_gnulinux@zoho.com>
2017-06-11 21:54:35 +0530

    added more packages

--- config.sh
+++ config.sh
@@ -2,5 +2,5 @@
 # config.sh: to override built in defaults
 
 EXTRA_PACKAGES+=(git)
-EXTRA_PACKAGES+=()
+EXTRA_PACKAGES+=(util-linux) # temp, should be added to manjaro-tools
 EXTRA_PACKAGES+=(manjaro-tools-pkg)
